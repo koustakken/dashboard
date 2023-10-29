3eac629b5833e8eb3bf0e7b3100aa530301c3b2f
Artem Ivanov <ivanov@hostco.ru>
2023-10-29 18:05:46 +0500

    add material table

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Task } from 'src/app/model/Task';
 import { DataHandlerService } from 'src/app/service/data-handler.service';
 
@@ -8,16 +9,30 @@ import { DataHandlerService } from 'src/app/service/data-handler.service';
 	styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+	public displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+	public dataSource!: MatTableDataSource<Task>;
+
 	tasks!: Task[];
 
 	constructor(private dataHandler: DataHandlerService) { }
 
 	ngOnInit(): void {
 		this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
-		console.log("@tasks ", this.tasks);
+		this.dataSource = new MatTableDataSource();
+		this.refreshTable();
 	}
 
 	toggleTaskCompleted(task: Task) {
 		task.completed = !task.completed;
+	}
+
+	public getPriorityColor(task: Task) {
+		if (task.priority && task.priority.color) {
+			return task.priority.color;
+		} return '#fff'
+	}
+
+	public refreshTable() {
+		this.dataSource.data = this.tasks;
 	}
 }
